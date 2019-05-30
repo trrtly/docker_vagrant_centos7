6ea22398843892eca61622344d2b6390ec121c53
su <su@zorzz.com>
2019-05-30 13:52:19 +0800

    perf: 初始化脚本里让用户输入配置

--- init.sh
+++ init.sh
@@ -1,4 +1,38 @@
 #!/usr/bin/env bash
 
-cp -i config.yaml.example config.yaml
-echo "初始化完成，请配置 config.yaml 后执行 'vagrant up'"
+cat <<EOF
+                      __
+  ___  ____ _      __/ /__
+ / _ \/ __ \ | /| / / //_/
+/  __/ /_/ / |/ |/ / ,<
+\___/ .___/|__/|__/_/|_|
+   /_/
+EOF
+
+basepath=$(cd `dirname $0`; pwd)
+configFile=${basepath}/config.yaml
+
+if [ -f ${configFile} ]; then
+    echo "配置文件已经存在，如有变更请编辑[${configFile}]后重启vagrant"
+    echo "正在启动 Vagrant..."
+    cd ${basepath}
+    vagrant up
+else
+    read -p "请输入[git config --global user.name]:" git_user
+    read -p "请输入[git config --global user.email]:" git_email
+    read -p "请输入挂载的路径，直接回车使用当前项目下的[data/webroot]:" webroot
+    if [ ! -n ${webroot} ]; then
+        webroot=data/webroot
+    fi
+
+    echo "正在生成配置文件[${configFile}]..."
+    cp ${configFile}.example ${configFile}
+    echo "system: linux">>${configFile}
+    echo "git_user: ${git_user}">>${configFile}
+    echo "git_email: ${git_email}">>${configFile}
+    echo "webroot: ${webroot}">>${configFile}
+
+    echo "正在启动 Vagrant..."
+    cd ${basepath}
+    vagrant up
+fi
